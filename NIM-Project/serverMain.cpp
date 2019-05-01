025d5efe0d0deaf9fd2f3d26fcac0e3c2a4677ab
Robert Thompson <43919584+robertThompson4@users.noreply.github.com>
2019-05-01 17:31:44 -0500

    Update serverMain.cpp

--- NIM-Project/serverMain.cpp
+++ NIM-Project/serverMain.cpp
@@ -1,19 +1,20 @@
 // server_main.cpp
 //   This function serves as the "main" function for the server-side
-#include "TicTacToe.h"
 #include <iostream>
 #include <string>
 #include <WinSock2.h>
+#include "nim.h"
+
 
 int serverMain(int argc, char *argv[], std::string playerName)
-{
+{	//
 	SOCKET s;
 	char buffer[MAX_RECV_BUFFER];
 	char host[v4AddressSize];
 	char port[portNumberSize];
 	char responseStr[MAX_SEND_BUFFER];
 
-	s = passivesock(TicTacToe_UDPPORT, "udp");
+	s = passivesock(NIM_UDPPORT, "udp");
 
 	std::cout << std::endl << "Waiting for a challenge..." << std::endl;
 	int len = UDP_recv(s, buffer, MAX_RECV_BUFFER, host, port);
@@ -21,28 +22,65 @@ int serverMain(int argc, char *argv[], std::string playerName)
 		std::cout << timestamp() << " - Received: " << buffer << " from " << host << ":" << port << std::endl;
 	}
 
+	/*if (strcmp(buffer, "Who?"))
+	{
+		char response[MAX_SEND_BUFFER] = "Your favorite Server";
+		UDP_send(s, response, MAX_SEND_BUFFER, host, port);
+	}*/
+
+
+
 	bool finished = false;
 	while (!finished) {
-		if (strcmp(buffer, TicTacToe_QUERY) == 0) {
+		if (strcmp(buffer, NIM_QUERY) == 0) {
 			// Respond to name query
-			strcpy_s(responseStr, TicTacToe_NAME);
+			strcpy_s(responseStr, NIM_NAME);
 			strcat_s(responseStr, playerName.c_str());
 			if (debug) {
 				std::cout << timestamp() << " - Sending: " << responseStr << " to " << host << ":" << port << std::endl;
 			}
 			UDP_send(s, responseStr, strlen(responseStr) + 1, host, port);
-
+			//
 		}
-		else if (strncmp(buffer, TicTacToe_CHALLENGE, strlen(TicTacToe_CHALLENGE)) == 0) {
+		else if (strncmp(buffer, NIM_CHALLENGE, strlen(NIM_CHALLENGE)) == 0) {
 			// Received a challenge  
-			char *startOfName = strstr(buffer, TicTacToe_CHALLENGE);
+			char *startOfName = strstr(buffer, NIM_CHALLENGE);
 			if (startOfName != NULL) {
-				std::cout << std::endl << "You have been challenged by " << startOfName + strlen(TicTacToe_CHALLENGE) << std::endl;
+				std::cout << std::endl << "You have been challenged by " << startOfName + strlen(NIM_CHALLENGE) << std::endl;
 			}
 
-			// Play the game.  You are the 'O' player
-			int winner = playTicTacToe(s, (char*)playerName.c_str(), host, port, PLAYER_O);
-			finished = true;
+			if (startOfName != NULL) {
+				bool checkWantToPlay = false;
+				std::cout << "Do you want to Play" << std::endl;
+				std::string answer = "";
+				std::cin >> answer;
+
+				for (std::string::size_type i = 0; i < answer.length(); ++i)
+					 toupper(answer[i]);
+
+				if (answer != "NO")
+				{
+					char xtraBuffer[MAX_RECV_BUFFER];
+					
+					UDP_send(s, NIM_SERVER_YES, strlen(NIM_SERVER_YES) + 1, host, port);
+					int status = wait(s, 20, 0);
+					UDP_recv(s, xtraBuffer, MAX_RECV_BUFFER, host, port);
+					
+					if (status > 0) {
+						if (strcmp(xtraBuffer, NIM_SERVER_GREAT) == 0)
+						{
+							// Play the game.  You are the 'O' player
+							int winner = playNim(s, playerName, host, port, 1);
+							finished = true;
+						}
+					}
+				}
+				else
+				{
+					char xtraBuffer[MAX_RECV_BUFFER]="Sorry Try again soon!";
+					UDP_send(s, xtraBuffer, strlen(NIM_SERVER_YES) + 1, host, port);
+				}
+			}
 		}
 
 		if (!finished) {
